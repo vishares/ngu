414d425138fa159d772273d26b82be4857f06605
vishares <vishares@gmail.com>
2018-01-18 20:22:19 +0530

    test

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   //app.use(express.static(__dirname + '/dist/')); 
 
 app.use('/zr',transaction)
-app.listen(process.env.OPENSHIFT_NODEJS_PORT  || 8080,process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',()=>{
-    console.log("Listening on " + process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' + ", server_port " + rocess.env.OPENSHIFT_NODEJS_PORT  || 8080)
+app.listen((process.env.OPENSHIFT_NODEJS_PORT  || 8080),(process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'),()=>{
+    console.log("Listening on " + (process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0') + ", server_port " + (process.env.OPENSHIFT_NODEJS_PORT  || 8080))
 })
 
